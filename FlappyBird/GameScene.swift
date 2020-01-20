1007b3d803c722ce439af4b9cfbd6c9a7142861e
katuya.kanatani <bouka.tenken@gmail.com>
2020-01-21 00:05:12 +0900

    1/20

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by USER on 2020/01/20.
+//  Copyright © 2020 katuya.kanatani. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    //SKView上にシーンが表示された時に呼ばれるメソッド
+    override func didMove(to view: SKView) {
+        //背景色を設定
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        //地面の画像を読み込む
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        //テクスチャを指定してスプライトを作成する
+        let groundSprite = SKSpriteNode(texture: groundTexture)
+        //スプライトの表示する位置を指定する
+        groundSprite.position = CGPoint(
+            x: groundTexture.size().width / 2,
+            y: groundTexture.size().height / 2 )
+        //シーンにスプライトを追加する
+        addChild(groundSprite)
+    }
+
+}
