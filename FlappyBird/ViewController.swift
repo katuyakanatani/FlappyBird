d273ae1031c4896b54641333b152a4c8e46eac9f
katuya.kanatani <bouka.tenken@gmail.com>
2020-01-20 23:36:18 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by USER on 2020/01/20.
+//  Copyright © 2020 katuya.kanatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
