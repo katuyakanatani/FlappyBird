e477f186cfabbcecfa460ed67913140811a18d32
katuya.kanatani <bouka.tenken@gmail.com>
2020-01-22 21:56:35 +0900

    鳥の前まで

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -26,7 +26,12 @@ class ViewController: UIViewController {
         //ビューにシーンを表示する
         skView.presentScene(scene)
     }
-
+    //ステータスバーを消す
+    override var prefersStatusBarHidden: Bool  {
+        get{
+            return true
+        }
+    }
 
 }
 
